d694af045fd0c5332293483e884cf848c4f4c3d8
Pramod Mali <57476794+pramali@users.noreply.github.com>
2025-02-04 12:41:53 +0530

    Update KafkaConstant.java

--- 19-SBMS-Kafka-App/src/main/java/com/ashokit/constant/KafkaConstant.java
+++ 19-SBMS-Kafka-App/src/main/java/com/ashokit/constant/KafkaConstant.java
@@ -6,6 +6,8 @@ public class KafkaConstant {
 	public static final String GROUP_ID="group_customer";
 	public static final String HOST="localhost:9092";
         public static final String DefaultCertificatePassword="AAaatt@22ee";
+	public static final String aws_access_key_id="AKIAEXAMPLE";
+        public static final String  aws_secret_access_key="SECRETEXAMPLE";
 	
 
 }
